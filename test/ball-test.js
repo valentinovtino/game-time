b033ead505724eb139f229d1722b194aca954742
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2017-12-07 00:13:55 -0700

    Write functions to test the Ball class and refactor move function

--- test/ball-test.js
+++ test/ball-test.js
@@ -1,6 +1,7 @@
 const assert = require('chai').assert;
 const Ball = require('../lib/Ball.js');
 const Paddle = require('../lib/Paddle.js');
+const Game = require('../lib/Game.js')
 
 let ball;
 let paddle;
@@ -10,6 +11,7 @@ describe('Ball', function() {
   beforeEach(() => {
     paddle = new Paddle(((800 - 70)/2), 475, 70, 20);
     ball = new Ball(((800 - 70)/2), 425, 0, 0, 6, 0, 2 * Math.PI, 0, 4);
+    game = new Game('canvas', 'context', 800, 500);
   })
 
   it('should be a function', function() {
@@ -27,5 +29,25 @@ describe('Ball', function() {
   it('should have a radius', function() {
     assert.equal(ball.radius, 6);
   });
+
+  it('should reverse direction when it hits sides of the wall', function() {
+    ball.dx = -1;
+    assert.equal(ball.dx, -1);
+    ball.wallCollision;
+    if(ball.x < 0) {
+      assert.equal(ball.dx, 1);
+    }
+  })
+
+  it('should move at the speed of it\'s .dx value', function() {
+    ball.dx = 5;
+    ball.x = 100;
+    assert.equal(ball.dx, 5);
+    assert.equal(ball.x, 100);
+
+    ball.move();
+
+    assert.equal(ball.x, 105);
+  })
 
 })
